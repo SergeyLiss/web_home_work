e6b401464915f156e3e35fc8ee406cdd20cc79ec
SergeyR <slog18122018@gmail.com>
2023-02-03 23:37:30 +0300

    added lesson_web_1-2

---
+++ lesson_web_1-2/assets/js/script.js
@@ -0,0 +1,11 @@
+last_user = 'Что-то'
+function VvodName(){
+    let name_user = prompt("Все таки нажал.\nИ как твое имя:", "имя")
+    alert(name_user)
+    Zamena(name_user)
+}
+
+function Zamena(name_user){
+    document.body.innerHTML = document.body.innerHTML.replace(new RegExp(last_user, 'g'), name_user);
+    last_user = name_user
+}
